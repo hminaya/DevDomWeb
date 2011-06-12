09c1d2779db1bc6d90aed6bc500d2f812363882b
Hector Minaya <hminaya@gmail.com>
2011-06-12 05:05:50 +0000

    tumblr.js

--- js/tmb.js
+++ js/tmb.js
@@ -9,28 +9,30 @@ function BuscarRelacionados(pstTag){
 		$("<span style='font-weight: bold;'>" + pstTag + "</span><ul id='" + pstTag+ "'></ul><br>").appendTo("#PostsRelacionados");
 		
 		$.each(tumblr_api_read.posts, function (indx, val){
+            window.arrPostsRelacionados.push(val["id"]);
 			$("<li><a href = '" + val["url-with-slug"] + "' title='" + val["regular-title"] + "'>" + val["regular-title"] + "</a></li>").appendTo("#" + pstTag);
-
 		});
 		
 	});
 }
 
+// FB - Login
 window.fbAsyncInit = function() {
     FB.init({appId: '231720456842175', status: true, cookie: true,
              xfbml: true});
   };
 
-//
+// On-Load
 $(function(){
 
     prettyPrint();
     
     (function() {
         var e = document.createElement('script'); e.async = true;
-        e.src = document.location.protocol +
-          '//connect.facebook.net/en_US/all.js';
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
         document.getElementById('fb-root').appendChild(e);
       }());
+      
+      window.arrPostsRelacionados = new Array();
 
 });
