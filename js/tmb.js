49dc169e3c275615aec2447af1a6c1994674d1f9
Hector Minaya <hminaya@gmail.com>
2011-06-12 06:00:43 +0000

    tumblr.js

--- js/tmb.js
+++ js/tmb.js
@@ -4,29 +4,32 @@ function BuscarRelacionados(pstTag){
     pstTag = $.trim(pstTag);
 
     if(pstTag){
-        if(!(document.location.href == "http://developers.do/")){
-            $.getScript("http://developers.do/api/read/json?tagged=" + pstTag, function(){
-                
-                // Crea el header el LI
-        		$("<span style='font-weight: bold;' id='spn" + pstTag + "'>" + pstTag + "</span><ul id='" + pstTag+ "'></ul><br>").appendTo("#PostsRelacionados");
-        		
-                // Colocar cada post
-        		$.each(tumblr_api_read.posts, function (indx, val){
-                    // Sin repetir
-                    if($.inArray(val["id"],window.arrPostsRelacionados) == -1){
-                        window.arrPostsRelacionados.push(val["id"]);
-            		    $("<li><a href = '" + val["url-with-slug"] + "' title='" + val["regular-title"] + "'>" + val["regular-title"] + "</a></li>").appendTo("#" + pstTag);
+        $.getScript("http://developers.do/api/read/json?tagged=" + pstTag, function(){
+            
+            // Crea el header el LI
+    		$("<span style='font-weight: bold;' id='spn" + pstTag + "'>" + pstTag + "</span><ul id='" + pstTag+ "'></ul><br>").appendTo("#PostsRelacionados");
+    		
+            // Colocar cada post
+    		$.each(tumblr_api_read.posts, function (indx, val){
+                // Sin repetir
+                if($.inArray(val["id"],window.arrPostsRelacionados) == -1){
+                    window.arrPostsRelacionados.push(val["id"]);
+                    if(!(document.location.href == "http://developers.do/")){
+                        $("<li><a href = '" + val["url-with-slug"] + "' title='" + val["regular-title"] + "'>" + val["regular-title"] + "</a></li>").appendTo("#" + pstTag);
                     }
-        		});
-                
-                // Borrar los tags si se quedao vacio
-                if($("#" + pstTag).children().size() == 0){
-                    $("#" + pstTag).hide();
-                    $("#spn" + pstTag).hide();
-                };
-        		
-        	});
-        }
+                    else{
+                        $("<li class='icon-liked'><a href = '" + val["url-with-slug"] + "' title='" + val["regular-title"] + "'>" + val["regular-title"] + "</a></li>").appendTo("#liked-box");
+                    }
+                }
+    		});
+            
+            // Borrar los tags si se quedao vacio
+            if($("#" + pstTag).children().size() == 0){
+                $("#" + pstTag).hide();
+                $("#spn" + pstTag).hide();
+            };
+    		
+    	});
     }
 }
 
