26fc7fcea5ba2af8cd1838fc61ffa805ef6ab586
Hector Minaya <hminaya@gmail.com>
2011-08-18 19:00:58 +0000

    mas data

--- js/devdom.js
+++ js/devdom.js
@@ -26,25 +26,89 @@
         "id": "1",
         "categoryName": "Java",
         "description": "Java",
-        "imageUrl": ""
+        "imageUrl": "",
+        "tutorials": [
+                {
+                    "id": "1",
+                    "name": "Construir librerías de JavaScript - Parte II",
+                    "description": "Construir librerías de JavaScript - Parte II",
+                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                }, 
+                {
+                    "id": "2",
+                    "name": "Construir librerías de JavaScript - Parte I",
+                    "description": "Construir librerías de JavaScript - Parte I",
+                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                },
+            ]
         },
     {
         "id": "2",
         "categoryName": "ASP.NET MVC",
         "description": "Model View Controller",
-        "imageUrl": ""
+        "imageUrl": "",
+        "tutorials": [
+                {
+                    "id": "1",
+                    "name": "Construir librerías de JavaScript - Parte II",
+                    "description": "Construir librerías de JavaScript - Parte II",
+                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                }, 
+                {
+                    "id": "2",
+                    "name": "Construir librerías de JavaScript - Parte I",
+                    "description": "Construir librerías de JavaScript - Parte I",
+                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                },
+            ]
         },
     {
         "id": "3",
         "categoryName": "Android",
         "description": "Android Development",
-        "imageUrl": ""
+        "imageUrl": "",
+        "tutorials": [
+                {
+                    "id": "1",
+                    "name": "Construir librerías de JavaScript - Parte II",
+                    "description": "Construir librerías de JavaScript - Parte II",
+                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                }, 
+                {
+                    "id": "2",
+                    "name": "Construir librerías de JavaScript - Parte I",
+                    "description": "Construir librerías de JavaScript - Parte I",
+                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                },
+            ]
         },
     {
         "id": "4",
         "categoryName": "WP7",
         "description": "Windows Phone 7",
-        "imageUrl": ""
+        "imageUrl": "",
+        "tutorials": [
+                {
+                    "id": "1",
+                    "name": "Construir librerías de JavaScript - Parte II",
+                    "description": "Construir librerías de JavaScript - Parte II",
+                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                }, 
+                {
+                    "id": "2",
+                    "name": "Construir librerías de JavaScript - Parte I",
+                    "description": "Construir librerías de JavaScript - Parte I",
+                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                },
+            ]
         }
     ]
 }    
