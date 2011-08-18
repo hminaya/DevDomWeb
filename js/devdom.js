eff352b34fd4f2269d6fe9412513c6ed4aee916f
Hector Minaya <hminaya@gmail.com>
2011-08-18 18:56:45 +0000

    ok

--- js/devdom.js
+++ js/devdom.js
@@ -1,50 +1,50 @@
 {"categorias":
 [
     {
-        id: "0",
-        categoryName: "jQuery",
-        description: "jQuery",
-        imageUrl: "",
-        tutorials: [
+        "id": "0",
+        "categoryName": "jQuery",
+        "description": "jQuery",
+        "imageUrl": "",
+        "tutorials": [
                 {
-                    id: "1",
-                    name: "Construir librerías de JavaScript - Parte II",
-                    description: "Construir librerías de JavaScript - Parte II",
-                    tutorialUrl: "http://tumblr.com/xq32xl4kxt",
-                    imageUrl: "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                    "id": "1",
+                    "name": "Construir librerías de JavaScript - Parte II",
+                    "description": "Construir librerías de JavaScript - Parte II",
+                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
                 }, 
                 {
-                    id: "2",
-                    name: "Construir librerías de JavaScript - Parte I",
-                    description: "Construir librerías de JavaScript - Parte I",
-                    tutorialUrl: "http://tumblr.com/xq32w83tvd",
-                    imageUrl: "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                    "id": "2",
+                    "name": "Construir librerías de JavaScript - Parte I",
+                    "description": "Construir librerías de JavaScript - Parte I",
+                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
                 },
             ]
         },
     {
-        id: "1",
-        categoryName: "Java",
-        description: "Java",
-        imageUrl: ""
+        "id": "1",
+        "categoryName": "Java",
+        "description": "Java",
+        "imageUrl": ""
         },
     {
-        id: "2",
-        categoryName: "ASP.NET MVC",
-        description: "Model View Controller",
-        imageUrl: ""
+        "id": "2",
+        "categoryName": "ASP.NET MVC",
+        "description": "Model View Controller",
+        "imageUrl": ""
         },
     {
-        id: "3",
-        categoryName: "Android",
-        description: "Android Development",
-        imageUrl: ""
+        "id": "3",
+        "categoryName": "Android",
+        "description": "Android Development",
+        "imageUrl": ""
         },
     {
-        id: "4",
-        categoryName: "WP7",
-        description: "Windows Phone 7",
-        imageUrl: ""
+        "id": "4",
+        "categoryName": "WP7",
+        "description": "Windows Phone 7",
+        "imageUrl": ""
         }
     ]
 }    
