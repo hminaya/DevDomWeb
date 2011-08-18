c75607982ee422ead41aa887d1acb72e5c7231e3
Hector Minaya <hminaya@gmail.com>
2011-08-18 20:18:29 +0000

    mas data

--- js/devdom.js
+++ js/devdom.js
@@ -20,27 +20,41 @@
                     "tutorialUrl": "http://tumblr.com/xq32w83tvd",
                     "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
                 },
+                {
+                    "id": "3",
+                    "name": "Opciones para cargar jQuery desde el CDN",
+                    "description": "Opciones para cargar jQuery desde el CDN",
+                    "tutorialUrl": "http://tumblr.com/xq32vdhjvb",
+                    "imageUrl": "http://online.coombeshead.devon.sch.uk/news_events/10/images/options.jpg"
+                },
+                {
+                    "id": "4",
+                    "name": "Cargar jQuery desde el CDN de Google",
+                    "description": "Cargar jQuery desde el CDN de Google",
+                    "tutorialUrl": "http://tumblr.com/xq32uwl5jz",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                },
             ]
         },
     {
         "id": "1",
         "categoryName": "Java",
-        "description": "Java",
+        "description": "Tutoriales y articulos sobre Java",
         "imageUrl": "",
         "tutorials": [
                 {
                     "id": "1",
-                    "name": "Construir librerías de JavaScript - Parte II",
-                    "description": "Construir librerías de JavaScript - Parte II",
-                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
-                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                    "name": "Guía básica de OOP en Java: Clases",
+                    "description": "Guía básica de OOP en Java: Clases",
+                    "tutorialUrl": "http://tumblr.com/xq33d1srwk",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lnxpf4R6wb1qjthf4.jpg"
                 }, 
                 {
                     "id": "2",
-                    "name": "Construir librerías de JavaScript - Parte I",
+                    "name": "Guía básica de OOP en Java: Objetos",
                     "description": "Construir librerías de JavaScript - Parte I",
-                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
-                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                    "tutorialUrl": "http://tumblr.com/xq32xehi6j",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmk0gfOwan1qjthf4.jpg"
                 },
             ]
         },
@@ -52,17 +66,17 @@
         "tutorials": [
                 {
                     "id": "1",
-                    "name": "Construir librerías de JavaScript - Parte II",
-                    "description": "Construir librerías de JavaScript - Parte II",
-                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
-                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                    "name": "Creando un Proyecto en ASP.NET MVC",
+                    "description": "Creando un Proyecto en ASP.NET MVC",
+                    "tutorialUrl": "http://tumblr.com/xq330y5q9i",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmw1uyPqVW1qjthf4.jpg"
                 }, 
                 {
                     "id": "2",
-                    "name": "Construir librerías de JavaScript - Parte I",
-                    "description": "Construir librerías de JavaScript - Parte I",
-                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
-                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                    "name": "Primeros pasos en ASP.NET MVC",
+                    "description": "Primeros pasos en ASP.NET MVC",
+                    "tutorialUrl": "http://tumblr.com/xq32wmhrfw",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lmhia25eJ21qjthf4.jpg"
                 },
             ]
         },
@@ -74,18 +88,11 @@
         "tutorials": [
                 {
                     "id": "1",
-                    "name": "Construir librerías de JavaScript - Parte II",
-                    "description": "Construir librerías de JavaScript - Parte II",
-                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
-                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                    "name": "Introducción Desarrollo de Apps para Android",
+                    "description": "Introducción Desarrollo de Apps para Android",
+                    "tutorialUrl": "http://tumblr.com/xq33rfsdor",
+                    "imageUrl": "http://media.tumblr.com/tumblr_lp2fouzBbv1qjthf4.png"
                 }, 
-                {
-                    "id": "2",
-                    "name": "Construir librerías de JavaScript - Parte I",
-                    "description": "Construir librerías de JavaScript - Parte I",
-                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
-                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
-                },
             ]
         },
     {
@@ -96,17 +103,17 @@
         "tutorials": [
                 {
                     "id": "1",
-                    "name": "Construir librerías de JavaScript - Parte II",
-                    "description": "Construir librerías de JavaScript - Parte II",
-                    "tutorialUrl": "http://tumblr.com/xq32xl4kxt",
-                    "imageUrl": "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+                    "name": "MVVM en aplicaciones Silverlight, WPF y Windows Phone 7",
+                    "description": "MVVM en aplicaciones Silverlight, WPF y Windows Phone 7",
+                    "tutorialUrl": "http://tumblr.com/xq33p0h0gt",
+                    "imageUrl": "http://media.tumblr.com/tumblr_loulgtog3g1qjthf4.png"
                 }, 
                 {
                     "id": "2",
-                    "name": "Construir librerías de JavaScript - Parte I",
-                    "description": "Construir librerías de JavaScript - Parte I",
-                    "tutorialUrl": "http://tumblr.com/xq32w83tvd",
-                    "imageUrl": "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+                    "name": "Introducción al desarrollo de Apps para el Windows Phone 7",
+                    "description": "Introducción al desarrollo de Apps para el Windows Phone 7",
+                    "tutorialUrl": "http://tumblr.com/xq33meqsc1",
+                    "imageUrl": "http://media.tumblr.com/tumblr_loo2q9Slhw1qjthf4.jpg"
                 },
             ]
         }
