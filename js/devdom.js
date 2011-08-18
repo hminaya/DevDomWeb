22afaa942f1c8cae408d2540e1f333fadc097cdf
Hector Minaya <hminaya@gmail.com>
2011-08-18 15:40:20 +0000

    gson

--- js/devdom.js
+++ js/devdom.js
@@ -1,17 +1,48 @@
 [
     {
         id: "0",
-        title: "jQuery",
-        description: "jQuery es lo mejor"
+        categoryName: "jQuery",
+        description: "jQuery",
+        imageUrl: "",
+        tutoriales: [
+            {
+                id: "1",
+                name: "Construir librerías de JavaScript - Parte II",
+                description: "Construir librerías de JavaScript - Parte II",
+                tutorialUrl: "http://tumblr.com/xq32xl4kxt",
+                imageUrl: "http://media.tumblr.com/tumblr_lmk5kqlIXs1qjthf4.png"
+            }, 
+            {
+                id: "2",
+                name: "Construir librerías de JavaScript - Parte I",
+                description: "Construir librerías de JavaScript - Parte I",
+                tutorialUrl: "http://tumblr.com/xq32w83tvd",
+                imageUrl: "http://media.tumblr.com/tumblr_lmg3q56EQt1qjthf4.png"
+            },
+            ]
         },
     {
         id: "1",
-        title: "Java",
-        description: "quien es java"
+        categoryName: "Java",
+        description: "Java",
+        imageUrl: ""
         },
     {
         id: "2",
-        title: "MVC",
-        description: "Otra mas......"
+        categoryName: "ASP.NET MVC",
+        description: "Model View Controller",
+        imageUrl: ""
+        },
+    {
+        id: "3",
+        categoryName: "Android",
+        description: "Android Development",
+        imageUrl: ""
+        },
+    {
+        id: "4",
+        categoryName: "WP7",
+        description: "Windows Phone 7",
+        imageUrl: ""
         }
     ]
