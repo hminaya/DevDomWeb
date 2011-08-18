9f43941146712df5b21d2fc458820bb1fb89c610
Hector Minaya <hminaya@gmail.com>
2011-08-18 14:50:06 +0000

    S

---
+++ js/DevDomJSON.js
@@ -0,0 +1,12 @@
+[
+    {
+        id: "0",
+        title: "jQuery",
+        description: "jQuery es lo mejor"
+        },
+    {
+        id: "1",
+        title: "Java",
+        description: "quien es java"
+        }
+    ]
